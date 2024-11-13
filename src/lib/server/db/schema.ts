4a1e7ac2b1c39b78507fdfa1a20ec8749804fe3e
Aditya <52035504+smashfries@users.noreply.github.com>
2024-11-12 22:51:35 -0500

    updated user schema

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,6 +1,10 @@
-import { pgTable, serial, text, integer } from 'drizzle-orm/pg-core';
+import { pgTable, serial, text, integer, uuid, timestamp } from 'drizzle-orm/pg-core';
 
 export const user = pgTable('user', {
-	id: serial('id').primaryKey(),
-	age: integer('age')
+	id: uuid('id').primaryKey(),
+	email: text('email').notNull(),
+	name: text('name').notNull(),
+	passwordHash: text('password_hash'),
+	updatedAt: timestamp('updated_at').defaultNow().notNull(),
+	createdAt: timestamp('created_at').defaultNow().notNull(),
 });
