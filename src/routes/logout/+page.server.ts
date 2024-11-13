183df069de95a5ef8e31233a80e2f1bc87412137
Aditya <52035504+smashfries@users.noreply.github.com>
2024-11-13 18:49:39 -0500

    added logic to update session info in db on logout

--- src/routes/logout/+page.server.ts
+++ src/routes/logout/+page.server.ts
@@ -1,8 +1,27 @@
+import * as jwt from 'jose';
 import { redirect } from '@sveltejs/kit';
 import type { Actions } from './$types';
+import { env } from '$env/dynamic/private';
+import { db } from '$lib/server/db';
+import { sessions } from '$lib/server/db/schema';
+import { eq } from 'drizzle-orm';
 
 export const actions = {
     default: async ({ cookies }) => {
+        const accessToken = cookies.get('accessToken')
+
+        if (accessToken) {
+            const secret = new TextEncoder().encode(env.SECRET)
+            try {
+                const {payload} = await jwt.jwtVerify(accessToken, secret)
+                if (payload) {
+                    await db.update(sessions).set({isActive: false}).where(eq(sessions.id, payload.sessionId as string))
+                }
+            } catch (e) {
+                console.log(e)
+            }
+        }
+
         cookies.delete('accessToken', { path: '/' })
         cookies.delete('refreshToken', { path: '/' })
 
