225cf91ca0191f947d9e5d9aaef66e1a66eba824
Aditya <52035504+smashfries@users.noreply.github.com>
2024-11-18 18:10:33 -0500

    changed adapter to node: bun adapter caused issue in prod

--- svelte.config.js
+++ svelte.config.js
@@ -1,5 +1,6 @@
 // import adapter from '@sveltejs/adapter-auto';
-import adapter from "svelte-adapter-bun";
+import adapter from '@sveltejs/adapter-node';
+// import adapter from "svelte-adapter-bun";
 import { vitePreprocess } from '@sveltejs/vite-plugin-svelte';
 
 /** @type {import('@sveltejs/kit').Config} */
@@ -11,8 +12,8 @@ const config = {
 		// adapter-auto only supports some environments, see https://svelte.dev/docs/kit/adapter-auto for a list.
 		// If your environment is not supported, or you settled on a specific environment, switch out the adapter.
 		// See https://svelte.dev/docs/kit/adapters for more information about adapters.
-		adapter: adapter()
-	}
+		adapter: adapter(),
+	},
 };
 
 export default config;
